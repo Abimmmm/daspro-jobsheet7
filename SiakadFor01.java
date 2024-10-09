c68d9803603dd3476f83ebce0e343e16cffe79ee
Abimmmm <156158397+Abimmmm@users.noreply.github.com>
2024-10-09 07:00:53 +0700

    Percobaan 1

--- SiakadFor01.java
+++ SiakadFor01.java
@@ -4,18 +4,32 @@ public class SiakadFor01 {
     public static void main(String[] args) {
         Scanner sc01 = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+        int batasKelulusan = 60; 
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
             nilai = sc01.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
-            }else if (nilai< terendah) {
+            }
+
+            if (nilai < terendah) {
                 terendah = nilai;
+            }
+
+            if (nilai >= batasKelulusan) {
+                lulus++; 
+            } else {
+                tidakLulus++; 
             }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
+
     }
 }
